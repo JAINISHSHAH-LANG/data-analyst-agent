0d5e1a7511effcec4646067b0608a965a6a1f7bd
Jainish Prathmesh Shah <24f1002326@ds.study.iitm.ac.in>
2025-08-16 16:01:57 +0530

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -1,16 +1,11 @@
 import requests
 
-# API endpoint
 url = "http://127.0.0.1:8000/api/"
 
-# Files to send
 files = {
-    "questions": open("questions.txt", "rb"),   # <-- make sure this file exists
-    "files": open("data.csv", "rb")             # <-- optional CSV file
+    "questions": open("questions.txt", "rb"),
+    "files": open("data.csv", "rb")
 }
 
-# Send POST request
 response = requests.post(url, files=files)
-
-# Print response JSON
 print(response.json())
